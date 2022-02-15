06dd303bb719fe3d206d0ffc3d725c97eb3428e2
OAS-DeSerT <18057332690@sina.cn>
2022-02-15 16:31:28 +0800

    Update nav1.py

--- nav1.py
+++ nav1.py
@@ -12,78 +12,78 @@ original = 0
 start = 1
 
 class NavToPoint:
-	def __init__(self):
-		rospy.on_shutdown(self.cleanup)
-		
-		# Subscribe to the move_base action server
-		self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-		rospy.loginfo("Waiting for move_base action server...")
-		
-		# Wait for the action server to become available
-		self.move_base.wait_for_server(rospy.Duration(120))
-		rospy.loginfo("Connected to move base server")
+    def __init__(self):
+        rospy.on_shutdown(self.cleanup)
 
-		nav_cmd_pub = rospy.Publisher("nav_cmd", String, queue_size=1)
+        # Subscribe to the move_base action server
+        self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+        rospy.loginfo("Waiting for move_base action server...")
 
-		# A variable to hold the initial pose of the robot to be set by the user in RViz
-		#initial_pose = PoseWithCovarianceStamped()
-		#rospy.Subscriber('initialpose', PoseWithCovarianceStamped, self.update_initial_pose)
-		quaternion = quaternion_from_euler(0.0, 0.0, 0.0)
-		self.origin = Pose(Point(0, 0, 0), Quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3]))	
-		# Get the initial pose from the user
-		#rospy.loginfo("*** Click the 2D Pose Estimate button in RViz to set the robot's initial pose...")
-		#rospy.wait_for_message('initialpose', PoseWithCovarianceStamped)
-		# Make sure we have the initial pose
-		#while initial_pose.header.stamp == "":
-		#	rospy.sleep(1)
-		rospy.loginfo("Ready to go")
-		rospy.sleep(1)
+        # Wait for the action server to become available
+        self.move_base.wait_for_server(rospy.Duration(120))
+        rospy.loginfo("Connected to move base server")
 
-		locations = dict()
+        nav_cmd_pub = rospy.Publisher("nav_cmd", String, queue_size=1)
 
-		# Location A 
-		A_x = -1.8
-		A_y = 3.4
-		A_theta = 1.57
-		quaternion = quaternion_from_euler(0.0, 0.0, A_theta)
-		locations['A'] = Pose(Point(A_x, A_y, 0.000), Quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3]))
-		
-		self.goal = MoveBaseGoal()
-		rospy.loginfo("Starting navigation test")
+        # A variable to hold the initial pose of the robot to be set by the user in RViz
+        #initial_pose = PoseWithCovarianceStamped()
+        #rospy.Subscriber('initialpose', PoseWithCovarianceStamped, self.update_initial_pose)
+        quaternion = quaternion_from_euler(0.0, 0.0, 0.0)
+        self.origin = Pose(Point(0, 0, 0), Quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3]))	
+        # Get the initial pose from the user
+        #rospy.loginfo("*** Click the 2D Pose Estimate button in RViz to set the robot's initial pose...")
+        #rospy.wait_for_message('initialpose', PoseWithCovarianceStamped)
+        # Make sure we have the initial pose
+        #while initial_pose.header.stamp == "":
+        #	rospy.sleep(1)
+        rospy.loginfo("Ready to go")
+        rospy.sleep(1)
 
-		while not rospy.is_shutdown():
-			self.goal.target_pose.header.frame_id = 'map'
-			self.goal.target_pose.header.stamp = rospy.Time.now()
+        locations = dict()
 
-			global start
-			# Robot will go to point A
-			if start == 1:
-				rospy.loginfo("Going to the dining table")
-				rospy.sleep(2)
-				self.goal.target_pose.pose = locations['A']
-				self.move_base.send_goal(self.goal)
-				waiting = self.move_base.wait_for_result(rospy.Duration(300))
-				if waiting == 1:
-					rospy.loginfo("Reached the dining table point")
-					rospy.sleep(2)
-					rospy.loginfo("Ready to work")
-					rospy.sleep(2)
-					nav_cmd = "done"
-					nav_cmd_pub.publish(nav_cmd)
-					start = 0
+        # Location A 
+        A_x = -1.8
+        A_y = 3.4
+        A_theta = 1.57
+        quaternion = quaternion_from_euler(0.0, 0.0, A_theta)
+        locations['A'] = Pose(Point(A_x, A_y, 0.000), Quaternion(quaternion[0], quaternion[1], quaternion[2], quaternion[3]))
 
-			rospy.Rate(5).sleep()
+        self.goal = MoveBaseGoal()
+        rospy.loginfo("Starting navigation test")
 
-	def update_initial_pose(self, initial_pose):
-		self.initial_pose = initial_pose
-		global original
-		if original == 0:
-			self.origin = self.initial_pose.pose.pose
-			original = 1
+        while not rospy.is_shutdown():
+            self.goal.target_pose.header.frame_id = 'map'
+            self.goal.target_pose.header.stamp = rospy.Time.now()
 
-	def cleanup(self):
-		rospy.loginfo("Shutting down navigation	....")
-		self.move_base.cancel_goal()
+            global start
+            # Robot will go to point A
+            if start == 1:
+                rospy.loginfo("Going to the dining table")
+                rospy.sleep(2)
+                self.goal.target_pose.pose = locations['A']
+                self.move_base.send_goal(self.goal)
+                waiting = self.move_base.wait_for_result(rospy.Duration(300))
+                if waiting == 1:
+                    rospy.loginfo("Reached the dining table point")
+                    rospy.sleep(2)
+                    rospy.loginfo("Ready to work")
+                    rospy.sleep(2)
+                    nav_cmd = "done"
+                    nav_cmd_pub.publish(nav_cmd)
+                    start = 0
+
+            rospy.Rate(5).sleep()
+
+    def update_initial_pose(self, initial_pose):
+        self.initial_pose = initial_pose
+        global original
+        if original == 0:
+            self.origin = self.initial_pose.pose.pose
+            original = 1
+
+    def cleanup(self):
+        rospy.loginfo("Shutting down navigation	....")
+        self.move_base.cancel_goal()
 
 if __name__=="__main__":
     rospy.init_node('navi_point')
@@ -92,6 +92,3 @@ if __name__=="__main__":
         rospy.spin()
     except:
         pass
-
-
-
